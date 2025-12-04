b43f71d923220d2e59fdcd043d5d142741c1cc6b
yacobelhaouari <yacobelhaouarii@gmail.com>
2025-12-04 18:19:36 +0100

    Splite func

--- func/splite.go
+++ func/splite.go
@@ -1,5 +1,20 @@
 package asciiart
 
-func Spite(fileName string) [][]string {
+import (
+	"os"
+	"strings"
+)
 
+func Splite(fileName string) [][]string {
+	var res [][]string
+	data, err := os.ReadFile(fileName)
+	if err != nil {
+		return [][]string{}
+	}
+	all := strings.Split(string(data), "\n")
+
+	for i := 1; i+7 < len(all); i += 9 {
+		res = append(res, all[i:i+8])
+	}
+	return res
 }
--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-
+import (
+	asciiart "asciiart/func"
+	"os"
+)
 
 func main() {
+	if len(os.Args) != 2 {
+		return
+	} else {
+		file := asciiart.Splite("standard.txt")
+		asciiart.PrintSymbole(file, os.Args[1])
+	}
 
 }
